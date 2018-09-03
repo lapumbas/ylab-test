9744bca3022a8e473a3fb0ecb97a8dc2573faca7
lapumbas <lapumbas@ya.ru>
2018-09-03 19:25:38 +0300

    object tree creation from initial data added

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,62 +1,40 @@
-import React, {Component, Fragment} from 'react';
-import {connect} from 'react-redux';
-import * as actions from '../../store/actions';
-import {Route, Router, Switch} from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
-import PropTypes from 'prop-types';
+import React, { Component, Fragment } from 'react';
+import _ from 'lodash';
 
-import "../../theme/style.less";
-
-import Home from '../home';
-import About from '../about';
-import Main from '../main';
-import Login from '../login';
-import NotFound from '../not-found';
-import Modals from '../modals';
+import data from '../../data/list.json';
 
 class App extends Component {
-
-  static propTypes = {
-    session: PropTypes.object.isRequired,
-    dispatch: PropTypes.func.isRequired,
-  };
-
   constructor(props) {
     super(props);
-    this.history = createBrowserHistory();
+  }
+
+  dataToTree(arr) {
+    const nodes = {};
+    return arr
+      .sort((a, b) => {
+        return a._id >= b._id;
+      })
+      .filter(item => {
+        const id = item['_id'];
+        const parentId = item['parent'];
+
+        nodes[id] = _.defaults(item, nodes[id], { children: [] });
+        parentId &&
+          (nodes[parentId] = nodes[parentId] || { children: [] })[
+            'children'
+          ].push(item);
+
+        return !parentId;
+      });
   }
 
   componentDidMount() {
-    this.props.dispatch(actions.session.remind());
+    console.log(this.dataToTree(data));
   }
 
   render() {
-    // If checking token
-    if (this.props.session.wait) {
-      return (
-        <Fragment>
-          Загрузка...
-        </Fragment>
-      );
-    }
-
-    return (
-      <Fragment>
-        <Router history={this.history}>
-          <Switch>
-            <Route path="/" exact={true} component={Home}/>
-            <Route path="/about" component={About}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/main" component={Main}/>
-            <Route component={NotFound}/>
-          </Switch>
-        </Router>
-        <Modals history={this.history}/>
-      </Fragment>
-    );
+    return <div>Hi</div>;
   }
 }
 
-export default connect(state => ({
-  session: state.session
-}))(App);
+export default App;
